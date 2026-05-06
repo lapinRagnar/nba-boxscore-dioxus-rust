80c3a9ffe876e0819d5359c21ab7f823b9f6e96f
lapinRagnar <lapinragnar@gmail.com>
2026-05-06 14:30:56 +0200

    hello world

--- src/main.rs
+++ src/main.rs
@@ -50,8 +50,13 @@ pub fn Hero() -> Element {
 #[component]
 fn Home() -> Element {
     rsx! {
-        Hero {}
-        Echo {}
+        div {
+            class: "flex items-center justify-center min-h-screen",
+            h1 {
+                class: "text-4xl font-bold text-blue-600",
+                "Hello World he e ! 🏀"
+            }
+        }
     }
 }
 
