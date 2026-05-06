0864eae73847bae4d354fff097a26b5664e9c932
lapinRagnar <lapinragnar@gmail.com>
2026-05-06 21:08:10 +0200

    12. src/components/navbar.rs

---
+++ src/components/navbar.rs
@@ -0,0 +1,22 @@
+use dioxus::prelude::*;
+use crate::Route;
+
+#[component]
+pub fn Navbar() -> Element {
+    rsx! {
+        div {
+            class: "bg-gray-800 text-white p-4 flex gap-4",
+            Link {
+                to: Route::Home {},
+                class: "hover:text-orange-400",
+                "🏠 Home"
+            }
+            Link {
+                to: Route::Blog { id: 1 },
+                class: "hover:text-orange-400",
+                "📝 Blog"
+            }
+        }
+        Outlet::<Route> {}
+    }
+}
