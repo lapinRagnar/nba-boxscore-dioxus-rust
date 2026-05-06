b6e3844c8dd7a6d4e26eb322a9032725182ad991
lapinRagnar <lapinragnar@gmail.com>
2026-05-06 21:04:55 +0200

    10. src/utils/helpers.rs

---
+++ src/utils/helpers.rs
@@ -0,0 +1,7 @@
+pub fn get_percentage(made: u8, attempted: u8) -> String {
+    if attempted == 0 {
+        return String::from("0%");
+    }
+    let pct = (made as f32 / attempted as f32) * 100.0;
+    format!("{:.0}%", pct)
+}
