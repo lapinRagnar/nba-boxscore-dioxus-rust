d3f470453292db1ab897038dcac6113fa4626cbf
lapinRagnar <lapinragnar@gmail.com>
2026-05-06 21:53:44 +0200

    2. Mise à jour de src/data/mod.rs :

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -1,5 +1,7 @@
 pub mod mock_games;
 pub mod mock_match;
+pub mod mock_regular_season_games;
 
 pub use mock_games::get_games;
 pub use mock_match::get_mock_match;
+pub use mock_regular_season_games::get_regular_season_games;
