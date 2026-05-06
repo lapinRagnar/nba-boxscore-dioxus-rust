862029226fca0ea8f3102c5c17111835f68bc6a4
lapinRagnar <lapinragnar@gmail.com>
2026-05-06 21:57:44 +0200

    4. Ajoute le composant à src/components/mod.rs :

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,7 +1,9 @@
 pub mod navbar;
 pub mod home;
 pub mod box_score;
+pub mod regular_season;  // ← Ajoute cette ligne
 
 pub use navbar::Navbar;
 pub use home::Home;
 pub use box_score::BoxScoreDisplay;
+pub use regular_season::RegularSeason;
